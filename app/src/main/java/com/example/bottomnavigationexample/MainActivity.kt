049e4ab5823467a503ddbbfa12676dbf66584cd1
Slowhand0309 <slowhand0309@gmail.com>
2023-09-19 07:02:28 +0900

    :tada: Initial project

---
+++ app/src/main/java/com/example/bottomnavigationexample/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.bottomnavigationexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bottomnavigationexample.ui.theme.ComposeBottomNavigationExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBottomNavigationExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyAppNavHost()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeBottomNavigationExampleTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigationexample/MyAppNavHost.kt
@@ -0,0 +1,19 @@
+package com.example.bottomnavigationexample
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MyAppNavHost(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "home"
+) {
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable("home") {
+            Greeting("Android")
+        }
+    }
+}
