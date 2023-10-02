5c954bb71c40127c1e4230872ec3f5b6163503c3
Slowhand0309 <slowhand0309@gmail.com>
2023-10-03 05:26:19 +0900

    :construction: Add main sceen nav graph

--- app/src/main/java/com/example/bottomnavigationexample/MyAppNavHost.kt
+++ app/src/main/java/com/example/bottomnavigationexample/MyAppNavHost.kt
@@ -1,19 +1,27 @@
 package com.example.bottomnavigationexample
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+
+private fun NavGraphBuilder.mainScreen() {
+    navigation(route = "main", startDestination = "main/entry") {
+        composable("main/entry") {
+            MainScreen()
+        }
+    }
+}
 
 @Composable
 fun MyAppNavHost(
     navController: NavHostController = rememberNavController(),
-    startDestination: String = "home"
+    startDestination: String = "main"
 ) {
     NavHost(navController = navController, startDestination = startDestination) {
-        composable("home") {
-            Greeting("Android")
-        }
+        mainScreen()
     }
 }
